ce952f5d04a4a1471e1859a06dd01633610fc587
Ahmad S. Salama <vadrif.draco@gmail.com>
2023-05-03 11:18:30 +0200

    Initial commit (pom.xml and environment)

---
+++ src/main/java/asu/foe/sketchy/SketchyApplication.java
@@ -0,0 +1,13 @@
+package asu.foe.sketchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/asu/foe/sketchy/SketchyApplicationTests.java
@@ -0,0 +1,13 @@
+package asu.foe.sketchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

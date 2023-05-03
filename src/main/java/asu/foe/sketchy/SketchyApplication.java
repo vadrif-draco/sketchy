e15b463d300fff24ea13eb7b5c05b3ab04c7f26d
nardine <nardine@Nardine>
2023-05-03 12:06:11 +0200

    Testing

--- src/main/java/asu/foe/sketchy/SketchyApplication.java
+++ src/main/java/asu/foe/sketchy/SketchyApplication.java
@@ -3,6 +3,7 @@ package asu.foe.sketchy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Testing Eclipse VC
 @SpringBootApplication
 public class SketchyApplication {
 

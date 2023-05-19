7fac3a9710cc5799c9dfb08992dcfaf5c236f41b
Ahmad S. Salama <vadrif.draco@gmail.com>
2023-05-19 23:53:30 +0300

    Update deprecated code in OCRService

--- src/main/java/asu/foe/sketchy/services/OCRService.java
+++ src/main/java/asu/foe/sketchy/services/OCRService.java
@@ -15,49 +15,42 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OCRService {
-	
-	public static String send(File file) throws IOException{
-		
-		OkHttpClient client = new OkHttpClient();
-		
-	    @JsonIgnoreProperties(ignoreUnknown = true)
-	    record ParsedResult(String ParsedText) {}
-	    @JsonIgnoreProperties(ignoreUnknown = true)
-	    record MyResponse(ArrayList<ParsedResult> ParsedResults) {}
 
-		
+	public static String send(File file, String fileName) throws IOException {
+
+		@JsonIgnoreProperties(ignoreUnknown = true)
+		record ParsedResult(String ParsedText) {}
+
+		@JsonIgnoreProperties(ignoreUnknown = true)
+		record MyResponse(ArrayList<ParsedResult> ParsedResults) {}
+
 		RequestBody reqBody = new MultipartBody.Builder()
-				.setType(MultipartBody.FORM)
-				.addFormDataPart("file", "try.png", RequestBody.create(MediaType.parse(""), file))
-				.addFormDataPart("language","eng")
-				.addFormDataPart("filetype", "png")
-				.addFormDataPart("detectOrientation", "true")
-				.addFormDataPart("OCREngine", "2")
-				.build();
-		
-		
+					.setType(MultipartBody.FORM)
+					.addFormDataPart("file", fileName, RequestBody.create(file, MediaType.parse("")))
+					.addFormDataPart("language", "eng")
+					.addFormDataPart("filetype", "png")
+					.addFormDataPart("detectOrientation", "true")
+					.addFormDataPart("OCREngine", "2")
+					.build();
+
 		Request req = new Request.Builder()
-				.url("https://api.ocr.space/parse/image")
-				.post(reqBody)
-				.header("apiKey", "d043c9198f88957")
-				.build();
-		
-		
-    try {
-        Response response = client.newCall(req).execute();
-        
-        System.out.println(response);
-        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-//        System.out.println(response.body().string());
+					.url("https://api.ocr.space/parse/image")
+					.post(reqBody)
+					.header("apiKey", "d043c9198f88957") // Nardine's API Key
+					.build();
 
-        MyResponse myResponse = new ObjectMapper().readValue(response.body().string(), MyResponse.class);
-        return myResponse.ParsedResults.get(0).ParsedText;
-    } catch (IOException e) {
-        e.printStackTrace();
-        return "error";
-    }
-}
+		OkHttpClient client = new OkHttpClient();
+
+		try {
+			Response response = client.newCall(req).execute();
+			System.out.println(response);
+			if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+			MyResponse myResponse = new ObjectMapper().readValue(response.body().string(), MyResponse.class);
+			return myResponse.ParsedResults.get(0).ParsedText;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
 
 }
-	
-
